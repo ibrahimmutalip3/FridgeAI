522efcb165692e7ccfd85a7166b4e4c3ed245be3
ibrahimmutalip3 <ibrahimmutalip3@gmail.com>
2026-08-29 15:13:35 +0000

    issjahqyg

--- fridge_ai/ios/Runner/AppDelegate.swift
+++ fridge_ai/ios/Runner/AppDelegate.swift
@@ -2,12 +2,19 @@ import Flutter
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // As of Flutter's UIScene lifecycle migration, plugins are no longer
+  // registered in didFinishLaunchingWithOptions. Instead they're registered
+  // here, once the implicit Flutter engine has been created.
+  // See: https://flutter.dev/to/uiscene-migration
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
